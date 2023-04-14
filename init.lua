f8f41ca34c6c2c6bfc2a25cb0b75d006f8b1bdc7
Patryk <ptrboguski@gmail.com>
2023-04-14 15:03:41 +0200

    small changes

--- init.lua
+++ init.lua
@@ -38,8 +38,18 @@ vim.api.nvim_create_autocmd("InsertLeave",{
         vim.cmd([[set relativenumber]])
 end})
 
-
-
-
-
-print("init.lua success")
+vim.o.clipboard = 'unnamedplus'
+--- for wsl nvim only
+vim.g.clipboard = {
+  name = 'WslClipboard',
+  copy = {
+    ['+'] = 'clip.exe',
+    ['*'] = 'clip.exe',
+  },
+  paste = {
+    ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}
+-- for normal linux find other way to share clipboard
--- lua/dns/init.lua
+++ lua/dns/init.lua
@@ -1,2 +1,1 @@
 require("dns.remap")
-print("lua/dns/init.lua success")
